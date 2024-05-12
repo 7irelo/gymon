2d1e5b85b9de6514f88e46f74987cfb850fcc8ae
7irelo <155379151+7irelo@users.noreply.github.com>
2024-05-12 21:07:21 +0200

    Initial commit

---
+++ Gymon/src/Gymon.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// For use by Gymon applications
+
+#include "Gymon/Application.h"
+#include "Gymon/Log.h"
+
+// ------- Entry Point ------------
+#include "Gymon/EntryPoint.h"
+
+// --------------------------------
---
+++ Gymon/src/Gymon/Log.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <memory>
+#include "Core.h"
+#include "spdlog/spdlog.h"
+
+namespace Gymon {
+
+	class GYMON_API Log
+	{
+	public:
+		static void Init();
+
+		inline static std::shared_ptr<spdlog::logger>& GetCoreLogger() { return s_CoreLogger; }
+		inline static std::shared_ptr<spdlog::logger>& GetClientLogger() { return s_ClientLogger; }
+	private:
+		static std::shared_ptr<spdlog::logger> s_CoreLogger;
+		static std::shared_ptr<spdlog::logger> s_ClientLogger;
+	};
+}
+
+
+// Core log macros
+#define GY_CORE_TRACE(...)  ::Gymon::Log::GetCoreLogger()->trace(__VA_ARGS__)
+#define GY_CORE_INFO(...)   ::Gymon::Log::GetCoreLogger()->info(__VA_ARGS__)
+#define GY_CORE_WARN(...)   ::Gymon::Log::GetCoreLogger()->warn(__VA_ARGS__)
+#define GY_CORE_ERROR(...)  ::Gymon::Log::GetCoreLogger()->error(__VA_ARGS__)
+#define GY_CORE_FATAL(...)  ::Gymon::Log::GetCoreLogger()->fatal(__VA_ARGS__)
+
+// Client log macros
+#define GY_TRACE(...)		::Gymon::Log::GetClientLogger()->trace(__VA_ARGS__)
+#define GY_INFO(...)		::Gymon::Log::GetClientLogger()->info(__VA_ARGS__)
+#define GY_WARN(...)		::Gymon::Log::GetClientLogger()->warn(__VA_ARGS__)
+#define GY_ERROR(...)		::Gymon::Log::GetClientLogger()->error(__VA_ARGS__)
+#define GY_FATAL(...)		::Gymon::Log::GetClientLogger()->fatal(__VA_ARGS__)
