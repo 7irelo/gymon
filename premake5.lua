f4e33f14da942a8184b152a5f3a063dd236d7f52
7irelo <tirelo.eric@gmail.com>
2024-10-02 00:42:36 +0200

    Premake 5

---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Gymon"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Gymon"
+	location "Gymon"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GY_PLATFORM_WINDOWS",
+			"GY_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "GY_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GY_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GY_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("obj/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Gymon/vendor/spdlog/include",
+		"Gymon/src"
+	}
+
+	links
+	{
+		"Gymon"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"GY_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "GY_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "GY_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "GY_DIST"
+		optimize "On"
