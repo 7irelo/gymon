bfd1f6b338bf1a33db8037ef153ac257760e7df5
eric-euphoria <eric.ncube@euphoria.co.za>
2024-11-23 23:31:03 +0200

    Window Abstraction

--- Gymon/src/Gymon/Core.h
+++ Gymon/src/Gymon/Core.h
@@ -10,4 +10,12 @@
 	#error Gymon only supports Windows!
 #endif
 
+#ifdef GY_ENABLE_ASSERTS
+	#define GY_ASSERT(x, ...) { if(!(x)) { GY_ERROR("Assersion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+	#define GY_CORE_ASSERT(X, ...) { if(!(x)) {GY_CORE_ERROR("Assersion Failed: {0}", __VA_ARGS__); __debugbreak(); } }
+#else
+	#define GY_ASSERT(x, ...)
+	#define GY_CORE_ASSERT(X, ...)
+#endif
+
 #define BIT(x) (1 << x)
